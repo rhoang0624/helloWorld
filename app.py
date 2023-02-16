d3e21451a1f8110b34f74ee265fdc43abd54333f
Ryan Hoang <rhoang@terpmail.umd.edu>
2023-02-15 21:00:31 -0500

    Modified to add a new route to About HTML

--- app.py
+++ app.py
@@ -4,12 +4,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def about():  # put application's code here
     return 'Hello World! from Ryan Hoang! I am adding my first code change.'
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 if __name__ == '__main__':
     app.run()
-
-<!-- This is my first comment -->
