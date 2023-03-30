ea421327802dda2957754e9563c7f2c2770128da
Ryan Hoang <rhoang@terpmail.umd.edu>
2023-03-30 18:53:14 -0400

    Created a Parent HTML to be utilized for child HTML pages, created appropriate routes for contact & favorite-course.

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():  # put application's code here
-    return 'Hello World! from Ryan Hoang! I am adding my first code change.'
+
+    fun_courses = ['BMGT302','BMGT402','BMGT407']
+
+    return render_template('hello.html', courses=fun_courses)
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+
+@app.route('/favorite-course')
+def favorite_course():
+    print('Subject: ' + request.args.get('subject'))
+    print('Course Number: ' + request.args.get('course_number'))
+
+    return render_template('favorite-course.html')
+
+
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 if __name__ == '__main__':
     app.run()
+
+
+
+
